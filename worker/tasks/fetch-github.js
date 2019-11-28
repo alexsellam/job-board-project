4b9ea47f68c5be8e26d2cc29e3ba92c7eff482d1
alexsellam <alex.sellam@gmail.com>
2019-11-28 17:56:37 +0100

    redis storage and basic filtering algo

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,4 +1,10 @@
 var fetch = require('node-fetch');
+var redis = require("redis"),
+    client = redis.createClient();
+
+const {promisify} = require('util');
+//const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 const baseURL = 'https://jobs.github.com/positions.json'
 
@@ -18,7 +24,28 @@ async function fetchGithub() {
         console.log('got', resultCount, 'jobs');
         onPage++;
     }
-
     console.log('got', allJobs.length, 'jobs total')
+    //filter algo
+
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+        if (
+            jobTitle.includes('senior') || 
+        jobTitle.includes('sr.') ||
+        jobTitle.includes('sr')  ||
+        jobTitle.includes('architect')
+        ) {
+            return false; 
+        }
+        return true;
+    })
+    console.log('filtered down to', jrJobs.length)
+    
+    //set in redis
+    const success = await setAsync('github', JSON.stringify(jrJobs));
+
+    console.log({success});
 }
+
+
 module.exports = fetchGithub
